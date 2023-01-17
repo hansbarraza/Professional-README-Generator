ac619b922dd1c09527a8971d448a0f5c77f15d2d
Hans Barraza <brain@Hanss-MacBook-Pro.local>
2023-01-16 20:46:52 -0700

    included packages and a question array and tested

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Every project must have a title.');
+                return false;
+            }
+        }
+    }];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeFile = data => {
+    fs.writeFile('README.md', data, err => {
+        if(err) {
+            console.log(err);
+            return;
+        } else {
+            console.log('Your README has been successfully created!')
+        }
+    })
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    return inquirer.prompt(questions);
+}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+init()
+
+.then(answers => {
+    return generateMarkdown(answers);
+})
+
+.then(data => {
+    return writeFile(data);
+})
+
+.catch(err => {
+    console.log(err)
+})
