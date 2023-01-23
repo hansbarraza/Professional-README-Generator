edf0de41e1dd7829f123ad52a3c189b9b0eb4e96
Hans Barraza <brain@Hanss-MacBook-Pro.local>
2023-01-22 21:51:33 -0700

    finished adding questions and functions for the generateMarkdown.js

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 
-// TODO: Create an array of questions for user input
+// Create an array of questions for user input
 const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('It is important to link your GitHub repo so users know where to find more of you work.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('It is important to provide email address for anyone with questions to contact you.');
+                return false;
+            }
+        }
+    },
     {
         type: 'input',
         name: 'title',
@@ -18,9 +44,89 @@ const questions = [
                 return false;
             }
         }
-    }];
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is your project description?',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('It is important to provide a detailed description of the project.');
+                return false;
+            }
+        }  
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the instructions for installation?',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('Provide instructions for installation to ensure users have proper software installed to run application.');
+                return false;
+            }
+        }  
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What are the instructions for usage?',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Providing instructions for usage will help users learn how to navigate application.');
+                return false;
+            }
+        }  
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'GNU', 'GPL'],
+        default: ['MIT'],
+        validate: licenseInput => {
+            if (licenseInput) {
+                return true;
+            } else {
+                return false;
+            }
+        }  
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can others contribute to this project?',
+        validate: contributingInput => {
+            if (contributingInput) {
+                return true;
+            } else {
+                console.log('Provide instructions on how other users can contribute to your project.');
+                return false;
+            }
+        }  
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Describe the tests in your application and how to use them.',
+        validate: testsInput => {
+            if (testsInput) {
+                return true;
+            } else {
+                console.log('Provide tests instructions.');
+                return false;
+            }
+        }  
+    },
+];
 
-// TODO: Create a function to write README file
+// Create a function to write README file
 const writeFile = data => {
     fs.writeFile('README.md', data, err => {
         if(err) {
@@ -32,12 +138,12 @@ const writeFile = data => {
     })
 };
 
-// TODO: Create a function to initialize app
+// Create a function to initialize app
 const init = () => {
     return inquirer.prompt(questions);
 }
 
-// // Function call to initialize app
+// Function call to initialize app
 init()
 
 .then(answers => {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,73 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  } else {
+    return `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`
+  }
+}
 
-// TODO: Create a function that returns the license link
+// Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return 'https://lbesson.mit-license.org/'
+  }
+  if (license === 'GNU') {
+    return 'https://gnu.org/licenses/licenses'
+  }
+  if (license === 'GPL') {
+    return 'http://perso.crans.org/besson/LICENSE.html'
+  } else {
+    return '';
+  }
+}
 
-// TODO: Create a function that returns the license section of README
+// Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  } else {
+    return `## Licenses
+    This project is covered under the ${license} license. To learn more click the license button at the top.`
+  }
+}
 
-// TODO: Create a function to generate markdown for README
+// Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ${renderLicenseSection(data.license)}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  If you have any questions about this project contact me directly at ${data.email}. You can also view more of my work at https://github.com/${data.github}.
 `;
 }
 
